b2d4a2bfac4605edf7142e738dfcbdd13cee645d
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-01-02 11:39:23 -0800

    Add lockboxes solution

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+    """
+      Function to determine if all boxes can be unlocked.
+      :param boxes: List of lists, where each sub-list contains keys to other boxes.
+      :return: True if all boxes can be unlocked, False otherwise.
+    """
+    boxes_unlocked = set([0])
+    boxes_queue = [0]
+
+    while boxes_queue:
+        current_boxes = boxes_queue.pop(0)
+
+        for key in boxes[current_boxes]:
+            if key not in boxes_unlocked and key < len(boxes):
+                boxes_unlocked.add(key)
+                boxes_queue.append(key)
+
+    return len(boxes_unlocked) == len(boxes)
