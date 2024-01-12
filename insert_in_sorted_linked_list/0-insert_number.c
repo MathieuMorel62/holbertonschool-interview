1d56c38e56315e1d2658bfff1c4d1f32424766ff
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-01-12 07:11:57 -0800

    Refactor insert_node function to improve readability and efficiency

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -8,29 +8,28 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-    if (head == NULL)
-        return (NULL);
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *current = *head;
 
-    listint_t *current = *head;
-    listint_t *new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    if (new_node == NULL)
-        return (NULL);
+	new_node->n = number;
 
-    new_node->n = number;
+	if (*head == NULL || number < (*head)->n)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-    if (current == NULL || number < current->n) {
-        new_node->next = current;
-        *head = new_node;
-        return (new_node);
-    }
+	while (current->next != NULL && current->next->n <= number)
+	{
+		current = current->next;
+	}
 
-    while (current->next != NULL && current->next->n < number) {
-        current = current->next;
-    }
+	new_node->next = current->next;
+	current->next = new_node;
 
-    new_node->next = current->next;
-    current->next = new_node;
-
-    return (new_node);
+	return (new_node);
 }
