bb787a3d43128a7947acda9ed8f96a79073834a1
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-12 21:46:50 +0000

    Updating the logging statistics script

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Parse logs and print statistics"""
-
 import sys
 
 file_sizes = [0]
@@ -12,7 +11,7 @@ http_status_count = {
     '403': 0,
     '404': 0,
     '405': 0,
-    '500': 0 
+    '500': 0
 }
 
 
@@ -23,20 +22,23 @@ def print_statistics():
         if count:
             print(f'{status_code}: {count}')
 
-try:
-    for line_number, line in enumerate(sys.stdin, start=1):
-        line_parts = line.rstrip().split()
-        try:
-            current_status_code = line_parts[-2]
-            current_file_size = line_parts[-1]
-            if current_status_code in http_status_count.keys():
-                http_status_count[current_status_code] += 1
-            file_sizes.append(int(current_file_size))
-        except Exception:
-            pass
-        if line_number % 10 == 0:
-            print_statistics()
-    print_statistics()
-except KeyboardInterrupt:
-    print_statistics()
-    raise
+
+if __name__ == "__main__":
+    """Parse logs and print statistics"""
+    try:
+        for line_number, line in enumerate(sys.stdin, start=1):
+            line_parts = line.rstrip().split()
+            try:
+                current_status_code = line_parts[-2]
+                current_file_size = line_parts[-1]
+                if current_status_code in http_status_count.keys():
+                    http_status_count[current_status_code] += 1
+                file_sizes.append(int(current_file_size))
+            except Exception:
+                pass
+            if line_number % 10 == 0:
+                print_statistics()
+        print_statistics()
+    except KeyboardInterrupt:
+        print_statistics()
+        raise
