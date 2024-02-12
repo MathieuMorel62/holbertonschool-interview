01e8b59ba92d41255d3e209472a8c1d7d334064e
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-12 17:51:49 +0000

    Update of the log statistics script to manage keyboard interruptions

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -23,30 +23,31 @@ def print_stats(file_size, status_counts):
         print(f"{status_code}: {status_counts[status_code]}")
 
 
-file_size = 0
-status_counts = defaultdict(int)
-line_count = 0
+if __name__ == "__main__":
+    file_size = 0
+    status_counts = defaultdict(int)
+    line_count = 0
 
-try:
-    for line in sys.stdin:
-        line = line.strip()
-        elements = line.split()
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            elements = line.split()
 
-        if len(elements) >= 5:
-            file_size += int(elements[-1])
-            status_code = elements[-2]
+            if len(elements) >= 5:
+                file_size += int(elements[-1])
+                status_code = elements[-2]
 
-            if status_code.isdigit():
-                status_counts[status_code] += 1
-            line_count += 1
+                if status_code.isdigit():
+                    status_counts[status_code] += 1
+                line_count += 1
 
-        if line_count == 10:
-            print_stats(file_size, status_counts)
-            line_count = 0
+            if line_count == 10:
+                print_stats(file_size, status_counts)
+                line_count = 0
 
-except KeyboardInterrupt:
+    except KeyboardInterrupt:
+        print_stats(file_size, status_counts)
+        sleep(random.random())
+        raise
+
     print_stats(file_size, status_counts)
-    sleep(random.random())
-    raise
-
-print_stats(file_size, status_counts)
