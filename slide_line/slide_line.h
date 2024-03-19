f153cb6d33ba114e7ae3255dbbf2284198d9addc
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-19 13:56:51 +0000

    Merge and slide left and right functions updated in slide_line.h

--- slide_line/slide_line.h
+++ slide_line/slide_line.h
@@ -5,13 +5,11 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-#define SLIDE_LEFT 0
+#define SLIDE_LEFT 2
 #define SLIDE_RIGHT 1
 
 int slide_line(int *line, size_t size, int direction);
-void slide_left(int *line, size_t size);
-void slide_right(int *line, size_t size);
-void merge_left(int *line, size_t size);
-void merge_right(int *line, size_t size);
+void merge_and_slide_left(int *line, size_t size);
+void merge_and_slide_right(int *line, size_t size);
 
 #endif /* __SLIDE_LINE_H__ */
