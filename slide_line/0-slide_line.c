ccc40bc034c4dae7572cd496a119f237c0c86904
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-19 13:57:08 +0000

    Updated merge and move left and right functions

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -1,90 +1,89 @@
 #include "slide_line.h"
 
 /**
- * merge_left - merges an array of integers to the left
- * @line: points to an array of integers containing size elements
- * @size: number of elements
-*/
-void merge_left(int *line, size_t size) {
+ * merge_and_slide_left - Slides and merges an array to the left.
+ * @line: Points to an array of integers containing size elements.
+ * @size: Number of elements in @line.
+ */
+void merge_and_slide_left(int *line, size_t size)
+{
+    size_t index = 0;
 	size_t i;
-    for (i = 0; i < size - 1; i++) {
-        if (line[i] != 0 && line[i] == line[i + 1]) {
-            line[i] *= 2;
-            line[i + 1] = 0;
-            i++;
-        }
-    }
-}
 
-/**
- * slide_left - slides an array of integers to the left
- * @line: points to an array of integers containing size elements
- * @size: number of elements
-*/
-void slide_left(int *line, size_t size) {
-    size_t j = 0;
-	size_t i;
-    for (i = 0; i < size; i++) {
-        if (line[i] != 0) {
-            if (i != j) {
-                line[j] = line[i];
+    for (i = 0; i < size; i++)
+	{
+        if (line[i] != 0)
+		{
+            if (index < i)
+			{
+                line[index] = line[i];
                 line[i] = 0;
             }
-            j++;
+            if (i + 1 < size && line[i + 1] == line[index])
+			{
+                line[index++] *= 2;
+                line[i + 1] = 0;
+            }
+			else
+			{
+                index++;
+            }
         }
     }
 }
 
 /**
- * slide_right - slides an array of integers to the right
- * @line: points to an array of integers containing size elements
- * @size: number of elements
-*/
-void slide_right(int *line, size_t size) {
-    size_t j = size - 1;
-	size_t i;
-    for (i = size - 1; i != (size_t)-1; i--) {
-        if (line[i] != 0) {
-            if (i != j) {
-                line[j] = line[i];
+ * merge_and_slide_right - Slides and merges an array to the right.
+ * @line: Points to an array of integers containing size elements.
+ * @size: Number of elements in @line.
+ */
+void merge_and_slide_right(int *line, size_t size)
+{
+    if (size == 0) return;
+
+    size_t index = size - 1;
+
+    for (int i = size - 1; i >= 0; i--)
+	{
+        if (line[i] != 0)
+		{
+            if ((size_t)i < index)
+			{
+                line[index] = line[i];
                 line[i] = 0;
             }
-            j--;
+            if (i - 1 >= 0 && line[i - 1] == line[index])
+			{
+                line[index--] *= 2;
+                line[i - 1] = 0;
+            }
+			else
+			{
+                index--;
+            }
         }
     }
 }
 
 /**
- * merge_right - merges an array of integers to the right
- * @line: points to an array of integers containing size elements
- * @size: number of elements
-*/
-void merge_right(int *line, size_t size) {
-	size_t i;
-    for (i = size - 1; i > 0; i--) {
-        if (line[i] != 0 && line[i] == line[i - 1]) {
-            line[i] *= 2;
-            line[i - 1] = 0;
-            i--;
-        }
+ * slide_line - Slides and merges an array of integers
+ * @line: Points to an array of integers containing size elements.
+ * @size: Number of elements in @line.
+ * @direction: Left or right.
+ * Return: 1 upon success, or 0 upon failure.
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+    if (direction == SLIDE_LEFT)
+	{
+        merge_and_slide_left(line, size);
     }
-}
-
-/**
- * slide_line - slides and merges an array of integers
- * @line: points to an array of integers containing size elements
- * @size: number of elements
- * @direction: left or right
- * Return: 1 upon success, or 0 upon failure
-*/
-int slide_line(int *line, size_t size, int direction) {
-    if (direction == SLIDE_LEFT) {
-        slide_left(line, size);
-        merge_left(line, size);
-    } else if (direction == SLIDE_RIGHT) {
-        slide_right(line, size);
-        merge_right(line, size);
-    } else {
+	else if (direction == SLIDE_RIGHT)
+	{
+        merge_and_slide_right(line, size);
+    }
+	else
+	{
         return 0;
     }
 
