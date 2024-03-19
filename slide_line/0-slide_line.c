43052ef352a7746f1ab1d4259d85ae2fd2ec2076
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-19 12:31:09 +0000

    Add drag and merge functions for the drag line

---
+++ slide_line/0-slide_line.c
@@ -0,0 +1,88 @@
+#include "slide_line.h"
+
+/**
+ * slide_left - slides an array of integers to the left
+ * @line: points to an array of integers containing size elements
+ * @size: number of elements
+*/
+void slide_left(int *line, size_t size) {
+    size_t j = 0;
+    for (size_t i = 0; i < size; i++) {
+        if (line[i] != 0) {
+            if (i != j) {
+                line[j] = line[i];
+                line[i] = 0;
+            }
+            j++;
+        }
+    }
+}
+
+/**
+ * merge_left - merges an array of integers to the left
+ * @line: points to an array of integers containing size elements
+ * @size: number of elements
+*/
+void merge_left(int *line, size_t size) {
+    for (size_t i = 0; i < size - 1; i++) {
+        if (line[i] != 0 && line[i] == line[i + 1]) {
+            line[i] *= 2;
+            line[i + 1] = 0;
+            i++;
+        }
+    }
+}
+
+/**
+ * slide_right - slides an array of integers to the right
+ * @line: points to an array of integers containing size elements
+ * @size: number of elements
+*/
+void slide_right(int *line, size_t size) {
+    size_t j = size - 1;
+    for (size_t i = size - 1; i != (size_t)-1; i--) {
+        if (line[i] != 0) {
+            if (i != j) {
+                line[j] = line[i];
+                line[i] = 0;
+            }
+            j--;
+        }
+    }
+}
+
+/**
+ * merge_right - merges an array of integers to the right
+ * @line: points to an array of integers containing size elements
+ * @size: number of elements
+*/
+void merge_right(int *line, size_t size) {
+    for (size_t i = size - 1; i > 0; i--) {
+        if (line[i] != 0 && line[i] == line[i - 1]) {
+            line[i] *= 2;
+            line[i - 1] = 0;
+            i--;
+        }
+    }
+}
+
+/**
+ * slide_line - slides and merges an array of integers
+ * @line: points to an array of integers containing size elements
+ * @size: number of elements
+ * @direction: left or right
+ * Return: 1 upon success, or 0 upon failure
+*/
+int slide_line(int *line, size_t size, int direction) {
+    if (direction == SLIDE_LEFT) {
+        merge_left(line, size);
+        slide_left(line, size);
+    } else if (direction == SLIDE_RIGHT) {
+        merge_right(line, size);
+        slide_right(line, size);
+    } else {
+        return 0;
+    }
+
+    return 1;
+}
