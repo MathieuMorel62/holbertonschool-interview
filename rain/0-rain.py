b00aa1a87088c153d9e0bf40f7b4fc40d32b3a86
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-05-01 14:17:06 +0000

    Implemented rainwater trapping calculation function in Python.

---
+++ rain/0-rain.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Rainwater problem """
+
+def rain(walls):
+    """ Calculate how much rainwater can be trapped """
+    if not walls:
+        return 0
+
+    rainwater = 0
+    for i in range(1, len(walls) - 1):
+        left = max(walls[:i])
+        right = max(walls[i+1:])
+        rainwater += max(0, min(left, right) - walls[i])
+
+    return rainwater
