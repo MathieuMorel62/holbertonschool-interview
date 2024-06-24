9dbece2aae5edcc79ee1c03e2222de66cabf8386
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-24 12:17:19 +0000

    Fix indent style in find_listint_loop function

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -5,27 +5,31 @@
  * find_listint_loop - finds the loop in a linked list
  * @head: pointer to the head of the list
  *
- * Return: address of the node where the loop starts, or NULL if there is no loop
+ * Return: address of the node where the loop starts, or NULL if is no loop
  */
-listint_t *find_listint_loop(listint_t *head) {
-    listint_t *slow = head;
-    listint_t *fast = head;
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-    /* Check if there is a loop */
-    while (fast != NULL && fast->next != NULL) {
-        slow = slow->next;
-        fast = fast->next->next;
+	/* Check if there is a loop */
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-        if (slow == fast) {
-            /* Find the start of the loop */
-            slow = head;
-            while (slow != fast) {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return slow;
-        }
-    }
+		if (slow == fast)
+		{
+			/* Find the start of the loop */
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
 
-    return NULL;
+	return (NULL);
 }
