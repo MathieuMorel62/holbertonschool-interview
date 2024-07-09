ca385d71b7e6765e8a411ffa7be467368758fed3
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-07-09 17:04:27 +0000

    chore: Refactor makeChange function for efficiency and readability

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -3,19 +3,22 @@
 
 
 def makeChange(coins, total):
+    """
+    Function that determines the fewest number of coins 
+    needed to meet a given amount total
+    """
     if total <= 0:
         return 0
 
-    # Initialisation du tableau dp
-    dp = [float('inf')] * (total + 1)
+    # Initialisation du tableau dp avec une valeur suffisamment grande
+    dp = [total + 1] * (total + 1)
     dp[0] = 0
 
     # Remplir le tableau dp
     for current_total in range(1, total + 1):
         for coin in coins:
             if coin <= current_total:
-                dp[current_total] = min(dp[current_total],
-                                        dp[current_total - coin] + 1)
+                dp[current_total] = min(dp[current_total], dp[current_total - coin] + 1)
 
     # Retourner le résultat
-    return dp[total] if dp[total] != float('inf') else -1
+    return dp[total] if dp[total] != total + 1 else -1
